c400123de8aec778ee259b9e720df5ef8c1004dc
SleepyLark <37351525+5kyl4rk@users.noreply.github.com>
2019-11-15 10:11:47 -0700

    CardController: Remove useless code

--- src/card/controller/CardController.java
+++ src/card/controller/CardController.java
@@ -16,31 +16,23 @@ import card.model.players.StandardPlayer.Type;
 public class CardController
 {
 
-	private StandardDealer luigi;
 	private GoFishMaster toad;
 	private WarMaster bowser;
-	private StandardPlayer playerOne;
-	private StandardPlayer playerTwo;
 	private GarbageMaster waluigi;
 
 	public CardController()
 	{
-		luigi = new StandardDealer(this);
 		toad = new GoFishMaster(this);
 		bowser = new WarMaster(this);
 		waluigi = new GarbageMaster(this);
-		playerTwo = new FishBot("Yoshi");
 		
-		luigi.buildDeck(false);
 
 	}
 
 	public void start()
 	{
+	
 		waluigi.startGame();
-		// consoleWar();
-		//consoleGoFishTest();
-		// dummyScenario();
 	}
 	
 	/**
@@ -51,77 +43,5 @@ public class CardController
 	{
 		System.out.println(message);
 	}
-
-	/*
-	 * Lol this thing doesn't work
-	 */
-	public void consoleWar()
-	{
-		Scanner consoleIn = new Scanner(System.in);
-		int turnCount = 0;
-		out("Enter name:");
-		playerOne = new StandardPlayer(consoleIn.nextLine());
-		luigi.shuffleCards();
-		luigi.shuffleCards();
-		bowser.addToGame(playerOne);
-		bowser.addToGame(playerTwo);
-		for (int times = 0; times < 26; times++)
-		{
-			playerOne.addToHand(luigi.draw());
-			playerTwo.addToHand(luigi.draw());
-		}
-
-		while (playerOne.getHandSize() > 0 && playerTwo.getHandSize() > 0)
-		{
-			out(bowser.battle(playerOne, playerTwo) + " wins the battle!");
-			turnCount++;
-		}
-
-		if (playerOne.getHandSize() > playerTwo.getHandSize())
-		{
-			out(playerOne + " is the victor!");
-
-		}
-		else
-		{
-			out(playerTwo + " is the victor");
-		}
-
-		out("Turns taken: " + turnCount);
-	}
-
-	/*
-	 * A method I used to first debug my initial logic, may delete later
-	 */
-	public void dummyScenario()
-	{
-		out("Current Players:");
-		out(toad.getPlayers() + "");
-		out(playerOne.getUsername() + "'s hand:");
-		out(playerOne.getCurrentHand());
-		out(playerTwo.getUsername() + "'s hand:");
-		out(playerTwo.getCurrentHand());
-		out(toad.currentPlayer() + " starts.");
-		playerOne.addToHand(luigi.draw());
-		out(toad.currentPlayer() + " draws a card.");
-		out(toad.currentPlayer() + " drew a(n) " + toad.currentPlayer().getCurrentHand().get(toad.currentPlayer().getHandSize() - 1));
-		out(toad.currentPlayer().getCurrentHand());
-		out(toad.currentPlayer() + " plays a(n) " + luigi.discard(toad.currentPlayer().discardCard(luigi.randomInt(0, toad.currentPlayer().getHandSize()))));
-		toad.next();
-		out("It's now " + toad.currentPlayer() + "'s turn");
-		out(toad.currentPlayer() + " draws a card.");
-		out(toad.currentPlayer() + " drew a(n) " + toad.currentPlayer().getCurrentHand().get(toad.currentPlayer().getHandSize() - 1));
-		out(toad.currentPlayer().getCurrentHand());
-		out(toad.currentPlayer() + " ends their turn.");
-		toad.next();
-		out("It is now " + toad.currentPlayer() + "'s turn.");
-		out(toad.currentPlayer() + " flips the table in rage.");
-		toad.next();
-		out("It looks like " + toad.currentPlayer() + " is the winner");
-		toad.currentPlayer().winner();
-		out(playerOne + ": " + playerOne.getWinCount());
-		out(playerTwo + ": " + playerTwo.getWinCount());
-
-	}
-
+	
 }
