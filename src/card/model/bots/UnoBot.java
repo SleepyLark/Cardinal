cb98543e7bcc4f067188ff7264382d9e9569da76
SleepyLark <37351525+5kyl4rk@users.noreply.github.com>
2019-12-05 13:29:08 -0700

    UNO: added stub classes for Player and Bot

---
+++ src/card/model/bots/UnoBot.java
@@ -0,0 +1,11 @@
+package card.model.bots;
+
+
+public class UnoBot extends Bot
+{
+	
+	public UnoBot(String username)
+	{
+		super(username);
+	}
+}
---
+++ src/card/model/players/UnoPlayer.java
@@ -0,0 +1,13 @@
+package card.model.players;
+
+public class UnoPlayer extends Player
+{
+
+	public UnoPlayer(String username)
+	{
+		super(username);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
