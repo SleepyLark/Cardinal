a6ecbb6229adeaf5d54e676ebb0d78191d974005
SleepyLark <37351525+5kyl4rk@users.noreply.github.com>
2019-12-07 17:29:57 -0700

    TrashBot: refractor

--- src/card/model/bots/TrashBot.java
+++ src/card/model/bots/TrashBot.java
@@ -3,7 +3,6 @@ package card.model.bots;
 import card.model.cards.Card;
 import card.model.cards.PlayingCard;
 import card.model.dealers.Dealer;
-import card.model.players.StandardPlayer;
 
 public class TrashBot extends Bot
 {
@@ -15,24 +14,24 @@ public class TrashBot extends Bot
 		super(username);
 
 	}
-	
-	public int turn(Card lastCardDiscarded, boolean[] currentHand )
+
+	public int turn(Card lastCardDiscarded, boolean[] currentHand)
 	{
-		if(currentHand[((PlayingCard)lastCardDiscarded).getNumber() - 1])
-		return TAKE_FROM_DISCARD;
+		if (currentHand[((PlayingCard) lastCardDiscarded).getNumber() - 1])
+			return TAKE_FROM_DISCARD;
 		else
 			return DRAW_A_CARD;
 	}
-	
+
 	public int processJack(boolean[] currentHand, int maxSize)
 	{
 		int cardSlot = 0;
-		
-		while(currentHand[cardSlot])
+
+		while (currentHand[cardSlot])
 			cardSlot = Dealer.randomInt(0, maxSize);
-		
+
 		return cardSlot;
-		
+
 	}
-	
+
 }
