d455fd4c27394678d014237da86a7975061d65f1
SleepyLark <37351525+5kyl4rk@users.noreply.github.com>
2019-12-15 21:10:29 -0700

    UnoBot: started work on logic method
    just a stub for now,  may need to change the method name to something that makes more sense

--- src/card/model/bots/UnoBot.java
+++ src/card/model/bots/UnoBot.java
@@ -1,11 +1,23 @@
 package card.model.bots;
 
+import card.model.cards.Card;
 
 public class UnoBot extends Bot
 {
 	
+	public static int DRAW_A_CARD = -1;
 	public UnoBot(String username)
 	{
 		super(username);
+	}
+	
+	/**
+	 * determines which card to play
+	 * @param cardInDiscard
+	 * @return the index of the card it wants to play, '-1' means to draw a card
+	 */
+	public int processTurn(Card cardInDiscard)
+	{
+		return DRAW_A_CARD;
 	}
 }
