a341514475f70a2be0492c495e77ef5ba6a605c8
SleepyLark <37351525+5kyl4rk@users.noreply.github.com>
2019-12-15 21:13:43 -0700

    UNO: started work on game logic
    unfortunately, the ternary function in the 'next()' method wasn't working, so I had to use 'if' statments instead (so much for trying to make it simple)

--- src/card/model/games/UnoMaster.java
+++ src/card/model/games/UnoMaster.java
@@ -1,9 +1,13 @@
 package card.model.games;
 
 import card.controller.CardController;
+import card.model.cards.Card;
+import card.model.cards.UnoCard;
 import card.model.dealers.UnoDealer;
+import card.model.players.Player;
 import card.model.players.UnoPlayer;
 import card.model.players.UnoPlayer.Type;
+import card.model.bots.UnoBot;
 
 public class UnoMaster extends GameMaster
 {
@@ -16,9 +20,8 @@ public class UnoMaster extends GameMaster
 	{
 		super();
 		this.app = app;
-		reverseOrder = false;
+		reverseOrder = true;
 		deck = new UnoDealer(app);
-		
 
 	}
 
@@ -27,6 +30,70 @@ public class UnoMaster extends GameMaster
 	{
 		setupGame();
 		
+		boolean gameOver = false;
+		app.out("Game start!");
+		while (!gameOver)
+		{
+			if(deck.isDrawDeckEmpty())
+			{
+				app.out("Out of cards. Reshuffling....");
+				deck.reshuffleDiscardPile();
+			}
+			app.out("it's now " + currentPlayer());
+			
+			app.out("Discard Pile: " + deck.discardPeek());
+
+			if (!hasPlayableCard(currentPlayer()))
+			{
+				app.out(currentPlayer() + " can't play anything, they must draw a card");
+				deck.draw(currentPlayer());
+			}
+			else if (!currentPlayer().isBot())
+			{
+				printHand();
+
+				boolean acceptable = false;
+				while (!acceptable)
+				{
+					app.out("Type the slot of the card you want to play, or type \"draw\" to take a card");
+					String input = consoleIn.next();
+
+					if (input.equalsIgnoreCase("draw"))
+					{
+						app.out("You drew a card");
+						acceptable = true;
+					}
+					else if (this.validInt(input, 0, currentPlayer().getHandSize()))
+					{
+						int num = Integer.parseInt(input);
+
+						if (cardIsPlayable(currentPlayer().pickCard(num)))
+						{
+							app.out("You played the " + deck.discard(currentPlayer().playCard(num)));
+							acceptable = true;
+						}
+						else
+							app.out("You can't play that card! Try again.");
+
+					}
+				}
+			}
+			else
+			{
+				int response = ((UnoBot)currentPlayer()).processTurn(deck.discardPeek());
+				
+				if(response == UnoBot.DRAW_A_CARD)
+				{
+					app.out(currentPlayer() + " decides to draw a card.");
+				}
+				else
+				{
+					app.out(currentPlayer() + " played the " + deck.discard(currentPlayer().playCard(response)));
+				}
+			}
+			
+			this.next();
+		}
 
 	}
 
@@ -34,13 +101,17 @@ public class UnoMaster extends GameMaster
 	protected void setupGame()
 	{
 		deck.buildDeck();
-		
+
 		app.out("Enter name:");
 		UnoPlayer playerOne = new UnoPlayer(consoleIn.nextLine());
 		this.addToGame(playerOne);
+		this.addBotPlayers(3, Game.UNO);
+		
 		deck.shuffleCards();
-		deck.dealCards(playerOne, 7);
+		deck.dealCards(getPlayers(), 7);
 		playerOne.organizeHand(Type.COLOR);
+		
+		deck.discard(deck.draw());
 
 	}
 
@@ -52,17 +123,63 @@ public class UnoMaster extends GameMaster
 	@Override
 	public void next()
 	{
+
+		if(reverseOrder)
+			currentTurn--;
+		else
+			currentTurn++;
 		
-		currentTurn = reverseOrder ? currentTurn-- : currentTurn++;
-	
+		
 		if (currentTurn >= this.numberOfPlayers())
 		{
 			currentTurn = 0;
 		}
-		else if(currentTurn < 0)
+		else if (currentTurn < 0)
 		{
-			currentTurn = numberOfPlayers()-1;
+			currentTurn = numberOfPlayers() - 1;
 		}
 		turnCount++;
 	}
+
+	private boolean hasPlayableCard(Player playerToCheck)
+	{
+		boolean canPlay = false;
+
+		for (int index = 0; index < playerToCheck.getHandSize(); index++)
+		{
+			Card current = playerToCheck.pickCard(index);
+
+			canPlay = (cardIsPlayable(current) == true);
+		}
+
+		return canPlay;
+	}
+
+	private boolean cardIsPlayable(Card cardToPlay)
+	{
+		boolean canPlay = false;
+		UnoCard cardInPlay = (UnoCard) cardToPlay;
+		UnoCard lastPlayed = (UnoCard) deck.discardPeek();
+		if (cardInPlay.isWild())
+		{
+			canPlay = true;
+		}
+		else
+		{
+			canPlay = (cardInPlay.getType() == lastPlayed.getType() || cardInPlay.getColor() == lastPlayed.getColor());
+		}
+
+		return canPlay;
+
+	}
+
+	private void printHand()
+	{
+		app.out("Hand:");
+		for (int index = 0; index < currentPlayer().getHandSize(); index++)
+		{
+			app.out(index + ": " + currentPlayer().pickCard(index));
+		}
+	}
+
 }
